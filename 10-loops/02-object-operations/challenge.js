83c6a8bcc9406eb841e08e0268893e1f5161476c
edav04 <edav04@gmail.com>
2014-06-14 18:49:04 +0100

    loops work v2

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -18,4 +18,16 @@ module.exports.extend = function(dest,src) {
       return Obj2;
 }
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj,array1) {
+ 
+  for (var i=0; i < array1.length; i++) { 
+      
+    if (array[i] = obj) {
+      console.log(obj[i] + ' ' + array1[i])
+      console.log(obj);
+      console.log(array1);
+      return false
+    } 
+    else {return true}
+    }
+}
--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,27 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn,actionFn)
+{
+conditionalFn = function() { return false; };
+actionFn = function() { console.log("Hello!"); };
+counter = 10;
+conditionalFn = function() {
+  while (counter > 0) { 
+  console.log(counter);
+  counter--;
+  return counter >= 0; 
+  }
+  stream(conditionalFn, actionFn);
+};
 
-module.exports.sumNumbers = undefined;
+}
+
+module.exports.sumNumbers = function(arg1)
+{
+  var i = arg1.length;
+  var count = 0
+  var sum = 0
+  while (count < arg1.length)
+  {sum += arg1[count]
+  count ++}
+  return sum;
+}
 
